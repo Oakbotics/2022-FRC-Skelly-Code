5efbd7e69681317ea675270183b9bd0f3cb02164
MilindMaheshwari <maheshwarim297@gmail.com>
2022-01-25 18:23:54 -0500

    Made function that moves robot one inch using encoder(not taking in account west coast drive with 4 motors because I have no clue how to calculate that.). Also  changed from making speedControllerGroup of left and right side, to follow a master.

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -3,12 +3,24 @@ package frc.robot.subsystems;
 import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import java.util.ArrayList;
+
+import com.ctre.phoenix.motorcontrol.GroupMotorControllers;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+
+
+
 
 public class DriveTrain extends SubsystemBase {
     
+
     private WPI_TalonFX leftSecondaryMotor; 
     private WPI_TalonFX leftPrimaryMotor; 
     private WPI_TalonFX rightSecondaryMotor;
@@ -17,6 +29,10 @@ public class DriveTrain extends SubsystemBase {
     SpeedControllerGroup leftMotorGroup; 
     SpeedControllerGroup rightMotorGroup;
     
+    SpeedControllerGroup allMotorGroup;
+    
+    ArrayList<WPI_TalonFX> motors = new ArrayList<WPI_TalonFX>();
+
     public DriveTrain() {
         super();
 
@@ -25,9 +41,38 @@ public class DriveTrain extends SubsystemBase {
         rightSecondaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_RIGHT_SECONDARY_MOTOR);
         rightPrimaryMotor = new WPI_TalonFX(Constants.DriveConstants.CAN_ADDRESS_RIGHT_PRIMARY_MOTOR);
 
-        leftMotorGroup = new SpeedControllerGroup(leftPrimaryMotor, leftSecondaryMotor);
-        rightMotorGroup = new SpeedControllerGroup(rightPrimaryMotor, rightSecondaryMotor);
+        leftSecondaryMotor.follow(leftPrimaryMotor);
+        rightSecondaryMotor.follow(rightPrimaryMotor);        
+
+        allMotorGroup = new SpeedControllerGroup(leftSecondaryMotor, leftPrimaryMotor, rightSecondaryMotor, rightPrimaryMotor);
+
+        for (WPI_TalonFX wpi_TalonFX : motors) {
+            wpi_TalonFX.configFactoryDefault();
+            wpi_TalonFX.setSensorPhase(false);
+            wpi_TalonFX.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 30);
+        }
+
+        
     }
+
+
+
+    public void goForwardFt(Double distance){
+        leftPrimaryMotor.set(TalonFXControlMode.Position,  1);
+        rightPrimaryMotor.set(TalonFXControlMode.Position, distance*Constants.DriveConstants.ENCODER_TICKS_PER_INCH);
+    }
+
+    public void turnDegrees(Double degrees){
+        //Make this asynchronous
+        double testingCoeffecientForDegrees = 30;
+        leftPrimaryMotor.set(TalonFXControlMode.Position, testingCoeffecientForDegrees * degrees);
+        rightPrimaryMotor.set(TalonFXControlMode.Position, -(testingCoeffecientForDegrees*degrees));
+    }
+
+    public void goFowardPerc(float distance){
+        leftPrimaryMotor.set(distance);
+    }
+
 
     @Override
     public void periodic() {
