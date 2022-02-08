478baf92a8b971917f3ecb7fbb6858f531f64fd4
shidai287 <shidai287@gotvdsb.ca>
2022-02-07 19:26:29 -0500

    command made

---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,17 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+import java.util.function.DoubleSupplier;
+
+public class Drive extends CommandBase {
+    private final DriveTrain m_driveTrain;
+    private final DoubleSupplier m_forward;
+    private final DoubleSupplier m_rotation;
+
+    public Drive (DriveTrain DriveTrain, DoubleSupplier forward, DoubleSupplier rotation) {
+        m_driveTrain = DriveTrain;
+        m_forward = forward;
+        m_rotation = rotation;
+    }
+}
